db1294594eb0def8eb7dd4d17ecff5c05ff4729c
t-kurihara <takuya.kurihara00@gmail.com>
2018-08-08 23:42:32 +0900

    [add] event listener bind syntax.

---
+++ src/app5.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js'
+  },
+  methods: {
+    reverseMessage: function() {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
