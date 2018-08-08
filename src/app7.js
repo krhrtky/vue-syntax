1c2c892ae18b10bcc97eacfb50cb29e8de96b57b
t-kurihara <takuya.kurihara00@gmail.com>
2018-08-09 00:32:45 +0900

    [add] component method.

---
+++ src/app7.js
@@ -0,0 +1,17 @@
+'use strict'
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+const app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 0, text: 'Whatever else humans are supposed to eat' },
+    ]
+  }
+})
